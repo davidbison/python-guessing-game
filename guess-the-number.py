03e61b06827f9b3916c0607db5a610ce54bf7fec
davidbison <davidbisonhubert@gmail.com>
2016-01-27 19:25:03 -0800

    Add two different ranges for play

--- guess-the-number.py
+++ guess-the-number.py
@@ -5,25 +5,34 @@
 import simplegui
 import random
 
+
+# state of first run set to (1,100]
+upper_bound = 99
+
+
 # helper function to start and restart the game
 def new_game():
     # initialize global variables used in your code here
     global secret_number
-    secret_number = random.randint(0, 99)
+    secret_number = random.randint(0, upper_bound)
+    print "New game. Range is from 0 to", upper_bound + 1
     print secret_number
+    print
 
 
 # define event handlers for control panel
 def range100():
     # button that changes the range to [0,100) and starts a new game
-    global secret_number
-    secret_number = random.randint(0, 99)
+    global secret_number, upper_bound
+    upper_bound = 99
+    new_game()
 
 
 def range1000():
     # button that changes the range to [0,1000) and starts a new game
-    global secret_number
-    secret_number = random.randint(0, 999)
+    global secret_number, upper_bound
+    upper_bound = 999
+    new_game()
 
 
 def input_guess(guess):
@@ -32,6 +41,7 @@ def input_guess(guess):
     print "Guess was " + guess
 
     game_messages = ["Higher\n", "Lower\n", "Correct\n"]
+
     if int_guess < secret_number:
         print game_messages[0]
     elif int_guess > secret_number:
@@ -47,7 +57,7 @@ frame = simplegui.create_frame("Guessing Game", 200, 200)
 
 # register event handlers for control elements and start frame
 frame.add_button("Range is [0, 100)", range100, 150)
-frame.add_button("Range is [0, 1000)", range1000, 15)
+frame.add_button("Range is [0, 1000)", range1000, 150)
 frame.add_input("Enter a guess", input_guess, 150)
 
 
